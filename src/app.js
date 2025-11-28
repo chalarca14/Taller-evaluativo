85ee92ef8c9412b82d7630501ea1817a029cd5cd
Sofia-Quintero <sofia67yyy@gmail.com>
2025-11-28 09:32:54 -0500

    chore: configure cors and rate limiting

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import authRoutes from "./routes/auth.js";
 import express from "express";
 import tasks from "./routes/tasks.js"
 import dotenv from "dotenv"
+import cors from "cors"
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const PORT = 3000;
 dotenv.config()
 
-
+app.use(express.json());
 
 app.use(express.json());
 
@@ -19,4 +21,26 @@ app.listen(PORT,()=>{
     console.log(`http://localhost:${PORT}`);
 })
 
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: ["GET","POST","PUT","DELETE"],
+        credentials: true
+    })
+);
+
+// Rate limiters
+
+const authLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 5,
+    message: "Demasiados intentos. Intenta de nuevo en 1 minuto."
+});
+
+//para limitar los /tasks
+const tasksLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 30,
+    message: "Demasiadas solicitudes a /tasks. Intenta mas tarde"
+});
 
